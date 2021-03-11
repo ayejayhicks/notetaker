ab90b3cae5a493a84992281f3656583437f83f06
Alaina Hicks <ajhicks0816@gmail.com>
2021-03-10 23:03:06 -0600

    routes, syntax error

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,26 +10,27 @@ module.exports =(app) => {
         res.send('<h1>Hi Alaina!!</h1>')
     })
 
+    app.get('/api/tables', (req, res) => res.json());
 
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
-    });
 
-    app.get('/api/notes',(req, res) => {
-        fs.readFile('./db/db.json', 'utf8', function(err, data){
-            res.send(data)
-        })
-    })
+//     app.get('/notes', (req, res) => {
+//         res.sendFile(path.join(__dirname, '../public/notes.html'));
+//     });
 
-    app.post('/api/notes', (req, res) => {
-        fs.readFile('./db/db.json', 'utf8', function(err, data) {
-        res.send("test")
-        console.log(req.body);
+//     app.get('/api/notes',(req, res) => {
+//         fs.readFile('./db/db.json', 'utf8', function(err, data) {
+//             res.send(data)
+//         })
+//     })
 
-        })
-    })
+//     app.post('/api/notes', (req, res) => {
+//         fs.readFile('./db/db.json', 'utf8', function(err, data) {
+//         res.send(data)
+//         console.log(req.body);
 
-    app.get('/', (req, res) => {
-       res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-};
+//         })
+//     })
+
+//     app.get('/', (req, res) => {
+//        res.sendFile(path.join(__dirname, '../public/index.html'));
+//     });
