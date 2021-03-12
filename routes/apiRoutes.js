15d626cbddf734e0fbaded47e9ae69a301a269d4
Alaina Hicks <ajhicks0816@gmail.com>
2021-03-11 20:56:49 -0600

    office hours help

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,84 @@
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
+const util = require('util');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 module.exports = (app) => {
 
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', function (err, data) {
-        res.send(data)
+    app.get('/api/notes', (req, res) => {
+        fs.readFile('./db/db.json', 'utf8', function (err, data) {
+            res.send(data)
         })
     });
 
-}
 
-app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    console.log(newNote);
-    try {
-        fs.readFile('./db/db.json', 'utf8', function (err, data) {
-            if (err) {
-                throw err;
-            };
-        });
-    }
-})
+    // app.post('/api/notes', (req, res) => {
+    //     const newNote = req.body;
+    //     console.log(newNote);
+    //     try {
+    //         readFileAsync('./db/db.json', 'utf8')
+    //         .then(function (err, data) {
+    //             if (err) {
+    //                 console.log("throwing....")
+    //                 throw err;
+    //             }
+    //             else {
+    //                 console.log(data);
+    //                 res.json(data);
+    //             }
+    //         });
+    //     } catch (error) {
+    //         console.log("we hit the catch!")
+    //         console.log(error);
+    //         res.status(500).json(error);
+    //     }
+        
+    // })
+
+    // app.post('/api/notes', (req, res) => {
+    //     const newNote = req.body;
+    //     console.log(newNote);
+
+    //         readFileAsync('./db/db.json', 'utf8')
+    //         .then((err, data) => {
+    //             if (err) {
+    //                 console.log("throwing....")
+    //                 throw err;
+    //             }
+    //             else {
+    //                 console.log(data);
+    //                 res.json(data);
+    //             }
+    //         })
+    //         .catch(err => res.status(500).send(err));
+        
+    // })
+
+    app.post('/api/notes', async (req, res) => {
+        const newNote = req.body;
+        console.log(newNote);
+    
+        //wrap our code in try/catch so we can handle any errors that may occur
+        try {
+            //use 'await' to handle our asynchronous reading of a file
+            const data = JSON.parse(await readFileAsync('./db/db.json', 'utf8'));  
+            
+            //do something with the data
+            data.push(req.body);
+
+            //write to the "db"
+            const result = await writeFileAsync('./db/db.json');
+            console.log(result);
+            
+            console.log(data);
+            res.json(data);
+    
+        } catch (err) {
+            console.log("we hit the catch!");
+            //if there was an error thrown, we handle it here
+            res.status(500).send(err);
+        }
+    });
+}
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,36 +1,36 @@
-const fs = require('fs');
-const path = require('path')
-const uuid = require ('uuid')
+// const fs = require('fs');
+// const path = require('path')
+// const uuid = require ('uuid')
 
-module.exports =(app) => {
+// module.exports =(app) => {
 
-//Test, walk through
+// //Test, walk through
 
-    app.get('/test', (req, res) => {
-        res.send('<h1>Hi Alaina!!</h1>')
-    })
-
-    app.get('/api/tables', (req, res) => res.json());
-
-
-//     app.get('/notes', (req, res) => {
-//         res.sendFile(path.join(__dirname, '../public/notes.html'));
-//     });
-
-//     app.get('/api/notes',(req, res) => {
-//         fs.readFile('./db/db.json', 'utf8', function(err, data) {
-//             res.send(data)
-//         })
+//     app.get('/test', (req, res) => {
+//         res.send('<h1>Hi Alaina!!</h1>')
 //     })
 
-//     app.post('/api/notes', (req, res) => {
-//         fs.readFile('./db/db.json', 'utf8', function(err, data) {
-//         res.send(data)
-//         console.log(req.body);
+//     app.get('/api/tables', (req, res) => res.json());
 
-//         })
-//     })
 
-//     app.get('/', (req, res) => {
-//        res.sendFile(path.join(__dirname, '../public/index.html'));
-//     });
+// //     app.get('/notes', (req, res) => {
+// //         res.sendFile(path.join(__dirname, '../public/notes.html'));
+// //     });
+
+// //     app.get('/api/notes',(req, res) => {
+// //         fs.readFile('./db/db.json', 'utf8', function(err, data) {
+// //             res.send(data)
+// //         })
+// //     })
+
+// //     app.post('/api/notes', (req, res) => {
+// //         fs.readFile('./db/db.json', 'utf8', function(err, data) {
+// //         res.send(data)
+// //         console.log(req.body);
+
+// //         })
+// //     })
+
+// //     app.get('/', (req, res) => {
+// //        res.sendFile(path.join(__dirname, '../public/index.html'));
+// //     });
